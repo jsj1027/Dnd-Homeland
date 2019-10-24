4333a2733675b4b31cc5bf7bea128de6fd2a48ee
Javis Johnson <Javis.Johnson@orolia.com>
2019-10-24 09:55:29 -0400

    clean up

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,3 @@ fn main() {
     println!("Proficienes: {:#?}", rand_race.proficienes);
     println!("Sizes: {:.1?}", rand_race.size);
 }
-// data_loader::connect(&pubber);
-// pubber.notify("select", "SELECT * FROM 'Clan Names'");
-
-// let dbpath = "./data/dnd.db";
-// let conn = Connection::open(&dbpath)?;
-// println!("{}", conn.is_autocommit());
-// match conn.execute("SELECT * FROM Race",NO_PARAMS) {
-//     Ok(answer) => answer,
-//     Err(_) => 1,
-// };
-// let mut statment = conn.prepare("SELECT * FROM 'Clan Names'").unwrap();
-// let prep = statment.query_map(NO_PARAMS, |row| Ok(ClanName {name: row.get(0)?, race: row.get(1)?}))?;
-// for item in prep {
-//     println!("Found Clan Name {:#?}",  item.unwrap());
-// }
-// Ok(())
--- src/race.rs
+++ src/race.rs
@@ -33,7 +33,6 @@ impl Race {
             size: get_size(&formatted_name, &data_base),
             name: name,
         }
-        // size: self.determine_size(name, full_race_data),
     }
 }
 
@@ -133,25 +132,3 @@ fn get_size(race_name: &str, data_base: &DatabaseConnection) -> (Size, f64) {
     
     (size_type, size_number)
 }
-
-// //     fn determine_size(&self, race: String, full_race_data: &serde_json::Map<String, serde_json::Value>) {
-// //         let current_race_data = &full_race_data[&race];
-// //         let max_size: &f64 = &current_race_data["maxSize"].as_f64().unwrap();
-// //         let min_size: &f64 = &current_race_data["minSize"].as_f64().unwrap();
-// //         let height = rand::thread_rng().gen_range(min_size, max_size);
-// //         let height: f64 = (height * 100.0).round() / 100.0;
-// //         println!("{}", height);
-// //         if height >= 1.0 && height < 2.0 {
-// //             self.size = (Size::Tiny, format!("{:.2} feet and inches", height))
-// //         } else if height >= 2.0 && height < 4.0 {
-// //             self.size = (Size::Small, format!("{:.2} feet and inches", height))
-// //         } else if height >= 4.0 && height < 8.0 {
-// //             self.size = (Size::Medium, format!("{:.2} feet and inches", height))
-// //         } else {
-// //             panic!(
-// //                 "Race size is either too big or too small. Size determined is {}",
-// //                 height
-// //             )
-// //         }
-// // }
-// }

a996524af4769bd27f8a8e1126e8fc678ee67121
Javis Johnson <Javis.Johnson@orolia.com>
2019-12-13 14:38:36 -0500

    feeling this so far.

--- src/class.rs
+++ src/class.rs
@@ -1,20 +1,38 @@
 use crate::data_connection::DatabaseConnection;
-use rusqlite::{NO_PARAMS};
-
-
+use rusqlite::{Row,Error, NO_PARAMS};
+use std::collections::HashMap;
 pub trait SqlStructure {
     fn get_struct_name() -> String;
 
-    fn new() {
+
+    fn new(class_name: &str)  {
         let data_base = DatabaseConnection::new();
-        let name = SqlStructure::get_struct_name();
-        let mut sql_query = String::from("SELECT * FROM Class WHERE name=") + &name;
-        let statement = data_base.connection.prepare(&sql_query[..]).unwrap();
-        let row = statement.query(NO_PARAMS).unwrap();
-        // let primary_stat: String = rows.next().unwrap().unwrap().get_unwrap(0);
-        
+        let struct_name = <Class as SqlStructure>::get_struct_name();
+        let sql_query = String::from("SELECT * FROM ") + &struct_name +" WHERE name=" + &class_name;
+        let mut statement = data_base.connection.prepare(&sql_query[..]).unwrap();
+        let mut row = statement.query(NO_PARAMS).unwrap();
+        let thing = row.next().unwrap().unwrap();
+        let primary_stat: String = thing.get_unwrap(0);
+        let s_stat: String = thing.get_unwrap(1);
+        let a_stat: String = thing.get_unwrap(2);
+        let c_stat: String = thing.get_unwrap(3);
+        println!("primstat {:#?}", primary_stat);
+        println!("primstat {:#?}", s_stat);
+        println!("primstat {:#?}", a_stat);
+        println!("primstat {:#?}", c_stat);
+    }
+
+    fn random_new() {
+        let data_base = DatabaseConnection::new();
+        let struct_name = <Class as SqlStructure>::get_struct_name();
+        let sql_query = String::from("SELECT * FROM ") + &struct_name +" ORDER BY RANDOM() LIMIT 1";
+        let mut statement = data_base.connection.prepare(&sql_query[..]).unwrap();
+        let mut row = statement.query(NO_PARAMS).unwrap();
+        let primary_stat: String = row.next().unwrap().unwrap().get_unwrap(0);
+        println!("primstat {:#?}", primary_stat);
     }
 }
+
 
 #[derive(Debug)]
 pub struct Class {
@@ -25,8 +43,8 @@ pub struct Class {
     pub cooperative_stat: String,
 }
 
-impl Class {
-    pub fn new() {
-
+impl SqlStructure for Class {
+    fn get_struct_name() -> String {
+        String::from("Class")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod class;
 mod data_connection;
 
 use class::Class;
+use class::SqlStructure;
 
 fn main() {
-    let rand_class = Class::new();
+    let rand_class = Class::new("'Barbarian'");
+    let rand_class = Class::random_new();
 }

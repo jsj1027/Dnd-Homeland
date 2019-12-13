516a36ff0d348fdb59bc3d076709b85c41c510de
Javis Johnson <Javis.Johnson@orolia.com>
2019-12-13 11:50:01 -0500

    sql again from some sort of scratch

--- src/class.rs
+++ src/class.rs
@@ -1,9 +1,8 @@
 use crate::data_connection::DatabaseConnection;
-use rand::{thread_rng, Rng};
-use rusqlite::{Row, Statement, NO_PARAMS};
+use rusqlite::{NO_PARAMS};
 
 
-trait SqlStructure {
+pub trait SqlStructure {
     fn get_struct_name() -> String;
 
     fn new() {
@@ -15,16 +14,6 @@ trait SqlStructure {
         // let primary_stat: String = rows.next().unwrap().unwrap().get_unwrap(0);
         
     }
-
-    // fn new_random() -> Self;
-
-    // fn add_string_to_query_end(query: &str, string: &str) -> String {
-    //    let mut query = String::from_str(query);
-    //    query = query + string;
-    //    query
-    // }
-        
-    // fn get_new_attribute(attribute: str) -> <T>;
 }
 
 #[derive(Debug)]
@@ -32,67 +21,12 @@ pub struct Class {
     pub name: String,
     pub primary_stat: String,
     pub secondary_stat: String,
-    // pub alternative_stat: String,
+    pub alternative_stat: String,
     pub cooperative_stat: String,
 }
 
 impl Class {
     pub fn new() {
 
-        // let data_base = DatabaseConnection::new();
-        // let name = get_name(&data_base);
-        // let formatted_name = get_formatted_name(&name);
-        // Class {
-        //     primary_stat: get_primary_stat(&formatted_name, &data_base),
-        //     secondary_stat: get_secondary_stat(&formatted_name, &data_base),
-        //     cooperative_stat: get_cooperative_stat(&formatted_name, &data_base),
-        //     name: name,
-        // }
     }
 }
-
-// fn get_name(data_base: &DatabaseConnection) -> String {
-//     let query = String::from("SELECT name FROM Class");
-//     let mut statement: Statement = data_base.connection.prepare(&query[..]).unwrap();
-//     let rows = statement.query_map(NO_PARAMS, |row| row.get(0)).unwrap();
-//     let mut results: Vec<String> = Vec::new();
-//     for row in rows {
-//         results.push(row.unwrap());
-//     }
-//     let mut rng = thread_rng();
-//     let index = rng.gen_range(0, results.len());
-//     String::from(&results[index])
-// }
-
-// fn get_formatted_name(unformatted_name: &str) -> String {
-//     let mut base = String::from("''");
-//     base.insert_str(1, unformatted_name);
-//     base
-// }
-
-// fn get_primary_stat(race_name: &String, data_base: &DatabaseConnection) -> String {
-//     let mut query: String = String::from("SELECT primaryStat FROM Class WHERE name=");
-//     query.push_str(&race_name);
-//     let mut statement = data_base.connection.prepare(&query[..]).unwrap();
-//     let mut rows = statement.query(NO_PARAMS).unwrap();
-//     let primary_stat: String = rows.next().unwrap().unwrap().get_unwrap(0);
-//     primary_stat
-// }
-
-// fn get_secondary_stat(race_name: &String, data_base: &DatabaseConnection) -> String {
-//     let mut query: String = String::from("SELECT secondaryStat FROM Class WHERE name=");
-//     query.push_str(&race_name);
-//     let mut statement = data_base.connection.prepare(&query[..]).unwrap();
-//     let mut rows = statement.query(NO_PARAMS).unwrap();
-//     let secondary_stat: String = rows.next().unwrap().unwrap().get_unwrap(0);
-//     secondary_stat
-// }
-
-// fn get_cooperative_stat(race_name: &String, data_base: &DatabaseConnection) -> String{
-//     let mut query: String = String::from("SELECT cooperativeStat FROM Class WHERE name=");
-//     query.push_str(&race_name);
-//     let mut statement = data_base.connection.prepare(&query[..]).unwrap();
-//     let mut rows = statement.query(NO_PARAMS).unwrap();
-//     let cooperative_stat = rows.next().unwrap().unwrap().get_unwrap(0);
-//     cooperative_stat
-// }
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,8 @@
 mod class;
 mod data_connection;
-mod race;
-mod status;
 
 use class::Class;
-use race::Race;
-use status::Status;
 
 fn main() {
-    // let rand_race = Race::new();
-    // println!("Race Name: {:#?}", rand_race.name);
-    // println!("Speed: {:#?}", rand_race.speed);
-    // println!("Age: {:#?}", rand_race.age);
-    // println!("Languages: {:#?}", rand_race.languages);
-    // println!("Proficienes: {:#?}", rand_race.proficienes);
-    // println!("Sizes: {:#?}, {:.1?}", rand_race.size.0, rand_race.size.1);
-    // println!("Bonus Stat: {:#?}\n\n", rand_race.stat_bonus);
-    // let mut new_status = Status::new(true);
-    // println!(
-    //     "strength score:{:#?}, modifer:{:#?}",
-    //     new_status.strength.score, new_status.strength.modifer
-    // );
-    // println!(
-    //     "dexterity score:{:#?}, modifer:{:#?}",
-    //     new_status.dexterity.score, new_status.dexterity.modifer
-    // );
-    // println!(
-    //     "constitution score:{:#?}, modifer:{:#?}",
-    //     new_status.constitution.score, new_status.constitution.modifer
-    // );
-    // println!(
-    //     "intelligence score:{:#?}, modifer:{:#?}",
-    //     new_status.intelligence.score, new_status.intelligence.modifer
-    // );
-    // println!(
-    //     "wisdom score:{:#?}, modifer:{:#?}",
-    //     new_status.wisdom.score, new_status.wisdom.modifer
-    // );
-    // println!(
-    //     "charisma score:{:#?}, charisma:{:#?}",
-    //     new_status.charisma.score, new_status.charisma.modifer
-    // );
-    // println!(
-    //     "\n\nInital strength score:{:#?}, modifer:{:#?}",
-    //     new_status.strength.score, new_status.strength.modifer
-    // );
-    // new_status.strength.change_by(&3);
-    // println!(
-    //     "Modded strength score:{:#?}, modifer:{:#?}",
-    //     new_status.strength.score, new_status.strength.modifer
-    // );
-    // new_status.strength.set_to(&20);
-    // println!(
-    //     "Set to strength score:{:#?}, modifer:{:#?}",
-    //     new_status.strength.score, new_status.strength.modifer
-    // );
-
     let rand_class = Class::new();
-    // println!("Class name: {:#?}", rand_class.name);
-    // println!("Class primary stat: {:#?}", rand_class.primary_stat);
-    // println!("Class secondary stat: {:#?}", rand_class.secondary_stat);
-    // println!("Class cooperative stat: {:#?}", rand_class.cooperative_stat);
 }

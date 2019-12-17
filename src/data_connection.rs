2ebfa045ea3e2fa8ad0e0c5b465331b7e9de60a7
jsj1027 <jsj1027@g.rit.edu>
2019-12-16 22:05:35 -0500

    remove dead code

--- src/data_connection.rs
+++ src/data_connection.rs
@@ -25,16 +25,3 @@ fn connect(path: &str) -> Result<Connection> {
         }
     }
 }
-// pub fn connect(ps: &PubSub) {
-//     let dbpath = "./data/dnd.db";
-//     let conn = Connection::open(&dbpath).unwrap();
-//     println!("Hello");
-
-//     let sub1 = ps.subscribe("select", move |query: String| {
-//         let mut state = conn.prepare(&query[..]).unwrap();
-//         let prep = state.query_map(NO_PARAMS, |row| Ok(ClanName {name: row.get(0).unwrap(), race: row.get(1).unwrap()})).unwrap();
-//         for item in prep {
-//             println!("Found Clan Name {:#?}",  item.unwrap());
-//         }
-//     });
-// }

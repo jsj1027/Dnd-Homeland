f5ce067204c9f958534bd9c297a664527251b098
jsj1027 <jsj1027@g.rit.edu>
2019-12-28 21:39:11 -0500

    building example

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-use crossterm::{
-    event::{self, Event as CEvent, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen},
-};
-use std::{
-    io::{stdout, Error, Write},
-    sync::mpsc,
-    thread,
-    time::Duration,
-};
-use tui::{backend::CrosstermBackend, Terminal};
+use std::io;
+use termion::raw::IntoRawMode;
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
 
-fn main() -> () {
-    let mut stdout = stdout();
-    execute!(stdout, EnterAlternateScreen);
-
-    let backend = CrosstermBackend::new(stdout);
-    
-    let mut terminal = Terminal::new(backend).unwrap();
-    terminal.hide_cursor().unwrap();
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|mut f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10)
+                ].as_ref()
+            )
+            .split(f.size());
+        Block::default()
+             .title("Block")
+             .borders(Borders::ALL)
+             .render(&mut f, chunks[0]);
+        Block::default()
+             .title("Block_2")
+             .borders(Borders::ALL)
+             .render(&mut f, chunks[2]);
+    })
 }

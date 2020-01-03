386000b7cecb18cbcdb7aabe26cc9ffb9667cb22
jsj1027 <jsj1027@g.rit.edu>
2020-01-03 18:45:38 -0500

    tutorial stuff

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use tui::backend::TermionBackend;
 use tui::widgets::{Widget, Block, Borders};
 use tui::layout::{Layout, Constraint, Direction};
 
+//https://github.com/fdehau/tui-rs/blob/master/examples/demo/app.rs
+// Good Example to study
+
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
